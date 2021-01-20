5e36eeeea156dbbdf92d91059ad19ddb460c2214
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2021-01-20 08:43:22 -0500

    Sistema de ventas

---
+++ src/ventas/Orden.java
@@ -0,0 +1,42 @@
+package ventas;
+
+public class Orden {
+    private final int idOrden;
+    private Producto[] productos;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+    private static int contadorPrdenes;
+
+    public Orden() {
+        this.idOrden = ++Orden.contadorPrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto) {
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS) {
+            this.productos[this.contadorProductos++] = producto;
+        }
+        else {
+            System.out.println("Se ha superado el máximo de productos: " + Orden.MAX_PRODUCTOS );
+        }
+    }
+
+    public double calcularTotal(){
+        double total = 0;
+        for (var i = 0; i < this.contadorProductos; i++) {
+            var producto = this.productos[i];
+            total += producto.getPrecio();
+        }
+        return total;
+    }
+
+    public void mostrarOrden(){
+        System.out.println("Id Orden: " + this.idOrden);
+        var totalOrden = this.calcularTotal();
+        System.out.println("\tTotal de la orden: $" + totalOrden);
+        System.out.println("\tProductos de la orden: ");
+        for (var i = 0; i < this.contadorProductos; i++) {
+            System.out.println("\t\t" + this.productos[i]);
+        }
+    }
+}
